b2f98f65b69a6464a29320bc80d5968d277d2aa1
Matthew Andres Moreno <m.more500@gmail.com>
2019-03-17 13:23:38 -0400

    Write tests

--- tests/test_keyname.py
+++ tests/test_keyname.py
@@ -7,7 +7,7 @@
 import unittest
 from click.testing import CliRunner
 
-from keyname import keyname
+from keyname import keyname as kf
 from keyname import cli
 
 
@@ -20,8 +20,89 @@ class TestKeyname(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
-        """Test something."""
+    def test_000_unpack(self):
+        """Test unpacking."""
+        goal = {
+            'seed' : '100',
+            'foobar' : '20',
+            '_hash' : 'asdf',
+            'ext' : '.txt'
+        }
+        assert kf.unpack("seed=100+foobar=20+_hash=asdf+ext=.txt") == goal
+
+        # reorderings
+        assert kf.unpack("foobar=20+seed=100+_hash=asdf+ext=.txt") == goal
+        assert kf.unpack("_hash=asdf+foobar=20+seed=100+ext=.txt") == goal
+
+        # missing and extra
+        assert kf.unpack("seed=100+_hash=asdf+ext=.txt") != goal
+        assert kf.unpack("seed=100+bz=bp+foobar=20+_hash=asdf+ext=.txt") != goal
+
+        # changed values
+        assert kf.unpack("seed=10+foobar=20+_hash=asdf+ext=.txt") != goal
+        assert kf.unpack("sed=100+foobar=20+_hash=asdf+ext=.txt") != goal
+        assert kf.unpack("sed=100+foobar=20+hash=asdf+ext=.txt") != goal
+
+        # should ignore path
+        assert kf.unpack("path/seed=100+foobar=20+_hash=asdf+ext=.txt") == goal
+        assert kf.unpack(
+            "~/more=path/+blah/seed=100+foobar=20+_hash=asdf+ext=.txt") == goal
+
+    def test_001_pack(self):
+        """Test packing."""
+
+        # reorderings
+        assert kf.pack({
+             'seed' : '100',
+             'foobar' : '20',
+             '_hash' : 'asdf',
+             'ext' : '.txt'
+         }) == "foobar=20+seed=100+_hash=asdf+ext=.txt"
+
+        assert kf.pack({
+             '_hash' : 'asdf',
+             'seed' : '100',
+             'foobar' : '20',
+             'ext' : '.txt'
+         }) == "foobar=20+seed=100+_hash=asdf+ext=.txt"
+
+        assert kf.pack({
+             '_hash' : 'asdf',
+             'foobar' : '20',
+             'ext' : '.txt',
+             'seed' : '100'
+         }) == "foobar=20+seed=100+_hash=asdf+ext=.txt"
+
+        # different values
+        assert kf.pack({
+             'seed' : '100',
+             'foobar' : 'blip',
+             '_hash' : 'asdf',
+             'ext' : '.txt'
+         }) == "foobar=blip+seed=100+_hash=asdf+ext=.txt"
+
+        assert kf.pack({
+             'seed' : 'a100',
+             'foobar' : 'blip',
+             '_hash' : 'asdf',
+             'ext' : '.txt'
+         }) == "foobar=blip+seed=a100+_hash=asdf+ext=.txt"
+
+        assert kf.pack({
+             'aseed' : 'a100',
+             'foobar' : 'blip',
+             '_hash' : 'asdf',
+             'ext' : '.txt'
+         }) == "aseed=a100+foobar=blip+_hash=asdf+ext=.txt"
+
+        # missing extension
+        assert kf.pack({
+             '_hash' : 'asdf',
+             'foobar' : '20',
+             'seed' : '100',
+         }) == "foobar=20+seed=100+_hash=asdf+ext=None"
+
+
 
     def test_command_line_interface(self):
         """Test the CLI."""
