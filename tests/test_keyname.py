9875084c45137138808f5e772b04d55a0431bb3f
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-12-20 23:19:29 -0500

    Fixup cli stub test

--- tests/test_keyname.py
+++ tests/test_keyname.py
@@ -166,7 +166,7 @@ class TestKeyname(unittest.TestCase):
         runner = CliRunner()
         result = runner.invoke(cli.main)
         assert result.exit_code == 0
-        assert 'keyname.cli.main' in result.output
+        assert 'Usage' in result.output
         help_result = runner.invoke(cli.main, ['--help'])
         assert help_result.exit_code == 0
         assert '--help  Show this message and exit.' in help_result.output
